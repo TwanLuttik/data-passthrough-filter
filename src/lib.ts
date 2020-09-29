b902b20c55f71e8aaff3dd178c87deae044a3449
Twan L <twanluttik@gmail.com>
2020-09-28 23:38:34 -0700

    if requireAll is not present, it will fail if you have a length check

--- src/lib.ts
+++ src/lib.ts
@@ -5,13 +5,11 @@ export const lengthCheck = (key: any, value: any, rule: any): any => {
   // filter for blacklisted properties 
   if (typeof value === 'object') return;
   
-  if (value.length < rule.length?.min) {
+  if (value?.length < rule.length?.min) {
     errors.push({ key, value, desc: `The minimun required length is ${rule.length.min}` });
-    // continue;
   } 
-  if (value.length > rule.length?.max) {
+  if (value?.length > rule.length?.max) {
     errors.push({ key, value, desc: `The maximun required length is ${rule.length.max}` });
-    // continue;
   }
 }
 
